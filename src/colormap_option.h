f5c98ec22fb388c5d3e2a615808ebef246e95528
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2020-08-21 08:52:48 +0900

    Added

---
+++ src/colormap_option.h
@@ -0,0 +1,7 @@
+#pragma once
+
+const char VIRIDIS_OPTION[] = "-v";
+const char PLASMA_OPTION[]  = "-p";
+const char INFERNO_OPTION[] = "-i";
+const char MAGMA_OPTION[]   = "-m";
+const char CIVIDIS_OPTION[] = "-c";
